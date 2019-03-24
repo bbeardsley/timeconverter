4a6f0c90556b1c723bd2f6ea6c409e0232d59a81
Brent Beardsley <brentbeardsley@gmail.com>
2019-03-24 10:41:49 -0600

    bump version

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"time"
 )
 
-const version = "0.1.0"
+const version = "0.2.0"
 
 func printUsage() {
 	fmt.Fprintln(os.Stderr, "Usage")
--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-version="0.1.0"
+version="0.2.0"
 package="timeconverter"
 bin="timeconverter"
 repo="https://github.com/bbeardsley/timeconverter"
